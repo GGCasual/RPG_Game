5eb74ceba4e66ec81186e54edb89e9569aec7e14
GGCasual <gc.favorito95@gmail.com>
2019-04-24 14:05:36 -0500

    first test

---
+++ Medieval.py
@@ -0,0 +1,9 @@
+class Hero():
+    def __init__(self, health, power):
+        self.health = health
+        self.power = power
+
+class Goblin():
+    def __init__(self, gobHealth, gobPower):
+        self.gobHealth = gobHealth
+        self.gobPower = gobPower
